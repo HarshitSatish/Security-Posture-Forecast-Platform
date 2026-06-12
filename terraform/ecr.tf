661722c7c64c8b07cdb04084978dca75f5f2e869
HARSHIT S <harshitskumar02@gmail.com>
2026-06-12 18:35:22 -0400

    feat: add ECR repository for API service

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -34,3 +34,15 @@ resource "aws_ecr_repository" "dashboard" {
 
   tags = { Name = "${var.project_name}-dashboard" }
 }
+
+resource "aws_ecr_repository" "api" {
+  name                 = "${var.project_name}-api"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = { Name = "${var.project_name}-api" }
+}
